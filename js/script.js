d160adcb83ddcb8cefac9136cc5365881c987d05
Kevin Tome <k.tome@unifesp.br>
2023-02-08 21:07:33 -0300

    feat: API data return

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ const apikey = '83f395c65961591fc7b706b1ed341a21';
 const cityInput = document.querySelector('#city-input');
 const searchBtn = document.querySelector('#search');
 
-const cityElement = document.querySelector('#city')
-const tempElement = document.querySelector('#temperature span')
-const descElement = document.querySelector('#description')
-const weatherIconElement = document.querySelector('#weather-icon')
-const umidityElement = document.querySelector('#umidity span')
-const windElement = document.querySelector('#wind span')
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+const weatherContainer = document.querySelector('#weather-data');
 
 //functions
 const getWeatherData = async(city) => {
@@ -17,11 +18,21 @@ const getWeatherData = async(city) => {
     const res = await fetch(apiWeatherURL)
     const data = await res.json();
 
-    console.log(data);
+    return data;
 } 
 
-const showWeatherData = (city) => {
-    getWeatherData(city)
+const showWeatherData = async(city) => {
+    const data = await getWeatherData(city);
+
+    console.log(data)
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    humidityElement.innerText = `${data.main.humidity}%`
+    windElement.innerText = `${data.wind.speed}km/h`
+
+    weatherContainer.classList.remove('hide');
 }
 
 //events
@@ -30,4 +41,12 @@ searchBtn.addEventListener("click", (e) => {
 
     const city = cityInput.value;
     showWeatherData(city)
+})
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code == "Enter") {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
 })
